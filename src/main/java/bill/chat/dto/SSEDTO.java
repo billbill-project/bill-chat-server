fae0a57e0412e776aed18cfaf4facb2e4be36ff4
jimmy0524 <10jmin04@naver.com>
2025-01-03 19:23:08 +0900

    [fix] sse 종료 로직 수정

--- src/main/java/bill/chat/dto/SSEDTO.java
+++ src/main/java/bill/chat/dto/SSEDTO.java
@@ -1,5 +1,6 @@
 package bill.chat.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,6 +12,7 @@ public class SSEDTO {
     String senderId;
     String content;
     int unreadCount;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     LocalDateTime updatedAt;
 
     public SSEDTO(String channelId, String senderId, String content, int unreadCount, LocalDateTime updatedAt) {
