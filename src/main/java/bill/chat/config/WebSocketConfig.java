94d74207306aced7038b6b65cdab062850d86d7b
jimmy0524 <10jmin04@naver.com>
2025-05-21 23:09:52 +0900

    [refactor] handlerMapping 메서드 구조 수정

--- src/main/java/bill/chat/config/WebSocketConfig.java
+++ src/main/java/bill/chat/config/WebSocketConfig.java
@@ -1,9 +1,9 @@
 package bill.chat.config;
 
+import bill.chat.service.CustomWebSocketService;
 import bill.chat.websocket.handler.AuthenticatedWebSocketHandler;
 import bill.chat.config.jwt.JWTUtil;
 import bill.chat.repository.ChatRoomRepository;
-import bill.chat.service.CustomWebSocketService;
 import bill.chat.websocket.handler.MyWebSocketHandler;
 
 import java.util.HashMap;
@@ -13,10 +13,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.socket.server.WebSocketService;
 import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.server.WebSocketService;
 import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
 import org.springframework.web.reactive.socket.server.upgrade.ReactorNettyRequestUpgradeStrategy;
 
@@ -33,13 +33,10 @@ public class WebSocketConfig {
     public HandlerMapping handlerMapping() {
         Map<String, WebSocketHandler> map = new HashMap<>();
         map.put("/ws/greeting", new AuthenticatedWebSocketHandler(myWebSocketHandler, jwtUtil, chatRoomRepository));
-
-        SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
-        mapping.setUrlMap(map);
-        mapping.setOrder(1);
+        int order = -1;
 
         log.info("Registering WebSocket handler for /ws/greeting");
-        return mapping;
+        return new SimpleUrlHandlerMapping(map, order);
     }
 
     @Bean
